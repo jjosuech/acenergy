c9a273035c9826768f375e112cb4dbeb3ceb4b26
jjosuech <josuechamba24@gmail.com>
2026-03-06 02:51:34 -0500

    corrigiendo api

--- src/main/java/com/pe/apiEnergy/security/SecurityConfig.java
+++ src/main/java/com/pe/apiEnergy/security/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth->auth
                         .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("api/servicios/**").permitAll()
+                        .requestMatchers("/api/servicios/**").permitAll()
                         .anyRequest().authenticated()
                 );
 
